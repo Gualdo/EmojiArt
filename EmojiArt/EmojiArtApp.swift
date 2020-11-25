2b787c96ca2b1f36efa375c41a5c5067abcf65eb
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2020-11-25 15:58:25 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by De La Cruz, Eduardo on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
